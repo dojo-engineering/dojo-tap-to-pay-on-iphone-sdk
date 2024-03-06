f85ca2b21bddc422fd460f406fb10d16832c7156
Den <deniss.kaibagarovs@paymentsense.com>
2024-03-06 23:12:44 +0000

    add sdk

---
+++ DojoTapToPayOniPhoneSDK.xcframework/ios-arm64/DojoTapToPayOniPhoneSDK.framework/Headers/DojoTapToPayOniPhoneSDK.h
@@ -0,0 +1,18 @@
+//
+//  DojoTapToPayOniPhoneSDK.h
+//  DojoTapToPayOniPhoneSDK
+//
+//  Created by Deniss Kaibagarovs on 06/03/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DojoTapToPayOniPhoneSDK.
+FOUNDATION_EXPORT double DojoTapToPayOniPhoneSDKVersionNumber;
+
+//! Project version string for DojoTapToPayOniPhoneSDK.
+FOUNDATION_EXPORT const unsigned char DojoTapToPayOniPhoneSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DojoTapToPayOniPhoneSDK/PublicHeader.h>
+
+
---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DojoTapToPayOniPhoneSDK",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(
+            name: "DojoTapToPayOniPhoneSDK",
+            targets: ["DojoTapToPayOniPhoneSDK"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "DojoTapToPayOniPhoneSDK", 
+            path: "DojoTapToPayOniPhoneSDK.xcframework"
+        )
+    ]
+)
